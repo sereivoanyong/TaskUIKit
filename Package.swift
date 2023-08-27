a64f5e140a85ec0924d092054f3146901f49d5f9
Sereivoan Yong <sereivoanyong@gmail.com>
2023-08-28 02:08:05 +0700

    Bump swift-tools-version to 5.8

--- Package.swift
+++ Package.swift
@@ -1,20 +1,21 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.8
+
 import PackageDescription
 
 let package = Package(
   name: "TaskUIKit",
   defaultLocalization: "en",
   platforms: [
-    .iOS(.v11),
+    .iOS(.v13),
   ],
   products: [
     .library(name: "TaskUIKit", targets: ["TaskUIKit"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/CoderMJLee/MJRefresh", .upToNextMajor(from: "3.7.5")),
-    .package(url: "https://github.com/sereivoanyong/SwiftUIKit", branch: "master"),
+    .package(url: "https://github.com/CoderMJLee/MJRefresh", .upToNextMajor(from: "3.7.6")),
+    .package(url: "https://github.com/sereivoanyong/UIKitSwift", branch: "main"),
   ],
   targets: [
-    .target(name: "TaskUIKit", dependencies: ["MJRefresh", .product(name: "UIKitExtra", package: "SwiftUIKit")]),
+    .target(name: "TaskUIKit", dependencies: ["MJRefresh", .product(name: "UIKitExtra", package: "UIKitSwift")]),
   ]
 )
