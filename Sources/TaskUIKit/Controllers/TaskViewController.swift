56b1a41962ec5989e41056e12e1650f9fe71c0b7
Sereivoan Yong <sereivoanyong@gmail.com>
2024-11-13 20:08:36 +0700

    Add `InitialAction`

--- Sources/TaskUIKit/Controllers/TaskViewController.swift
+++ Sources/TaskUIKit/Controllers/TaskViewController.swift
@@ -26,6 +26,12 @@ public enum TaskResult<Contents> {
 /// `applyData(_:source:)`
 open class TaskViewController<Contents>: UIViewController, EmptyViewStateProviding, EmptyViewDataSource {
 
+  public enum InitialAction {
+
+    case reload
+    case loadFromCacheOrReload
+  }
+
   public enum SourcedContents {
 
     case response(Contents, isInitial: Bool, TaskUserInfo? = nil)
@@ -77,10 +83,11 @@ open class TaskViewController<Contents>: UIViewController, EmptyViewStateProvidi
     return nil
   }
 
-  open private(set) var isLoading: Bool = false
+  /// If `initialError` is `nil`, this property is performed.
+  /// Set to `nil` to do nothing. We will have to manually reload task.
+  open var initialAction: InitialAction? = .reload
 
-  /// If `contents` isn't nil nor empty, this property is ignored. Setting this property after `viewIsAppearing(_:)` has no effect.
-  open var loadsTaskOnViewAppear: Bool = true
+  open private(set) var isLoading: Bool = false
 
   /// Returns the scroll view for pull-to-refresh
   open var refreshingScrollView: UIScrollView? {
@@ -147,14 +154,31 @@ open class TaskViewController<Contents>: UIViewController, EmptyViewStateProvidi
 
     if isFirstViewAppear {
       isFirstViewAppear = false
-      if initialError == nil {
-        if loadsTaskOnViewAppear {
-          reloadTasks(reset: true, animated: true)
-        } else {
-          emptyView.reload()
-        }
-      } else {
+      if initialError != nil {
         emptyView.reload()
+      } else {
+        if let initialAction {
+          switch initialAction {
+          case .reload:
+            reloadTasks(reset: true, animated: true)
+          case .loadFromCacheOrReload:
+            if let cachedContents, !isNilOrEmpty(cachedContents) {
+              applyData(.cache(cachedContents)) { [unowned self] in
+                emptyView.reload()
+
+#if !targetEnvironment(macCatalyst)
+                if let refreshingScrollView {
+                  if automaticallyConfiguresHeaderRefreshControl {
+                    configureHeaderRefreshControl(for: refreshingScrollView)
+                  }
+                }
+#endif
+              }
+            } else {
+              reloadTasks(reset: true, animated: true)
+            }
+          }
+        }
       }
     }
   }
