ea9cc30c11476ffad2e1c33b56649d7bca362f00
Sereivoan Yong <sereivoanyong@gmail.com>
2025-01-09 15:36:37 +0700

    Refactor `Paging`

--- Sources/TaskUIKit/Paging.swift
+++ Sources/TaskUIKit/Paging.swift
@@ -10,3 +10,24 @@ public protocol Paging {
 
   func hasNextPage() -> Bool
 }
+
+public protocol PageBasedPaging: Paging {
+
+  var page: Int { get }
+}
+
+extension Optional where Wrapped == Paging {
+
+  public var nextOrFirstPage: Int {
+    switch self {
+    case .none:
+      return 1
+    case .some(let paging):
+      if let paging = paging as? PageBasedPaging {
+        return paging.page + 1
+      }
+      assertionFailure("Attemp to access `nextOrFirstPage` on non-`PageBasedPaging`")
+      return 1
+    }
+  }
+}
